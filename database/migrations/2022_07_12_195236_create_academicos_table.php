2649a09222936ea2ca2d3200f80c30efd998392e
mak-1506 <claudiomag89@hotmail.com>
2022-07-12 20:54:03 -0500

    niveles

---
+++ database/migrations/2022_07_12_195236_create_academicos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcademicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('CtxDescripcion', 200)->nullable()->default('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academicos');
+    }
+}
